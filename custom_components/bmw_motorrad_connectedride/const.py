0288bf7bdf2161356087d4f312e1b512d75689e9
Memphius <wytzewissink@gmail.com>
2026-03-17 02:00:42 +0100

    Update authentication constants and scopes

--- custom_components/bmw_motorrad_connectedride/const.py
+++ custom_components/bmw_motorrad_connectedride/const.py
@@ -6,14 +6,12 @@ PLATFORMS = ["sensor", "binary_sensor", "device_tracker"]
 CONF_CLIENT_ID = "client_id"
 CONF_COUNTRY = "country"
 CONF_API_HOST = "api_host"
-CONF_DEVICE_CODE_HOST = "device_code_host"
-CONF_TOKEN_HOST = "token_host"
+CONF_AUTH_HOST = "auth_host"
 CONF_POLL_INTERVAL = "poll_interval"
 CONF_VERIFY_SSL = "verify_ssl"
 
 DEFAULT_API_HOST = "https://cpp.bmw-motorrad.com"
-DEFAULT_DEVICE_CODE_HOST = "https://api-cardata.bmwgroup.com"
-DEFAULT_TOKEN_HOST = "https://api-cardata.bmwgroup.com"
+DEFAULT_AUTH_HOST = "https://customer.bmwgroup.com"
 DEFAULT_COUNTRY = "nl-NL"
 DEFAULT_POLL_INTERVAL = 300
 
@@ -25,5 +23,8 @@ TOKEN_ENDPOINT = "/gcdm/oauth/token"
 BIKES_ENDPOINT_TMPL = "/v2/service/{country}/bmc-user-bikes"
 
 SCOPES = [
-    "cardata:api:read"
+    "authenticate_user",
+    "openid",
+    "cardata:streaming:read",
+    "cardata:api:read",
 ]
